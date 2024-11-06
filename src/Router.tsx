e03b9dd236645616990c2422b77d2a9545e04ee0
joonyoungchoi0801 <joeplay0801@gmail.com>
2024-11-06 22:54:24 +0900

    chore: styled 기본 세팅 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,11 +2,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import GlobalStyles from './styles/GlobalStyles';
 
+import Home from './pages/home';
+
 export default function Router() {
   return (
     <BrowserRouter>
       <GlobalStyles />
-      <Routes></Routes>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </BrowserRouter>
   );
 }
