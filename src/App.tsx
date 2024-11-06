18a8215dd1c546c001393330c810f319a7224a25
joonyoungchoi0801 <joeplay0801@gmail.com>
2024-11-06 22:50:34 +0900

    chore: router 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import Router from './Router';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
+      <Router />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
