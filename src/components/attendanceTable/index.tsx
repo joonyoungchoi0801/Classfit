8c4d4f0277e947a8ac1d0a83bf4f0b5ab3592dbe
roor <02tladbwjd@naver.com>
2024-11-08 18:45:02 +0900

    Feat: 학생 출결 테이블 생성

---
+++ src/components/attendanceTable/index.tsx
@@ -0,0 +1,13 @@
+import * as S from './AttendanceTable.styles';
+
+function AttendanceTable() {
+  return (
+    <div>
+      <S.SearchContainer>
+
+      </S.SearchContainer>
+    </div>
+  );
+}
+
+export default AttendanceTable;
