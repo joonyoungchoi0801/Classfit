be82d5ad94454b143347dc8eb28549d73ee20556
roor <02tladbwjd@naver.com>
2024-11-09 16:22:29 +0900

    Feat: 이름검색 input칸 구현

--- src/components/attendanceTable/AttendanceTable.styles.tsx
+++ src/components/attendanceTable/AttendanceTable.styles.tsx
@@ -1,5 +1,32 @@
 import styled from "styled-components";
 
-export const SearchContainer = styled.div`
-  
+export const TableHeader = styled.div`
+  display: flex;
+  padding: 0.625rem;
+  align-items: center;
+  gap: 1.375rem;
+  background: #F9F9F9;
+`;
+
+export const SeachContainer = styled.div`
+  display: flex;
+  flex-direction: space-between;
+  width: 10.875rem;
+  height: 2.5rem;
+  padding: 0.3125rem 1.25rem;
+  align-items: center;
+  gap: 0.625rem;
+  border-radius: 0.3125rem;
+  background: #EDEDED;
+`;
+
+export const SearchIcon = styled.img`
+  width: 1.125rem;
+  height: 1.125rem;
+`;
+
+export const SearchInput = styled.input`
+  width: 7.875rem;
+  border: transparent;
+  background-color: #EDEDED;
 `;
