c9d4339bee0ae5e82266be79bce5f1d43f84d888
roor <02tladbwjd@naver.com>
2024-11-09 16:21:24 +0900

    Feat: 월별 드롭다운 구현

--- src/pages/attendance/Attendance.styles.tsx
+++ src/pages/attendance/Attendance.styles.tsx
@@ -26,9 +26,54 @@ export const ButtonGroup = styled.div`
 
 export const LeftButtons = styled.div`
   display: flex;
+  gap: 1.25rem;
 `;
 
 export const RightButtons = styled.div`
   display: flex;
   gap: 1.25rem;
 `;
+
+export const DownloadContainer = styled.div`
+  position: relative; 
+  display: inline-block;
+`;
+
+export const DropdownButton = styled.button`
+  padding: 0.94rem 1.25rem; 
+  border: none; 
+  color: #0056F6;
+  cursor: pointer;
+
+  img {
+    width: 1.5rem;
+    height: 1.5rem;
+  }
+`;
+
+export const DropdownList = styled.ul`
+  position: absolute; 
+  top: 100%; 
+  left: 0; 
+  background-color: #fff; 
+  border: 1px solid #ccc; 
+  list-style: none; 
+  padding: 0; 
+  margin: 0; 
+  z-index: 1;
+`;
+
+export const DropdownItem = styled.li` 
+  padding: 10px; 
+  cursor: pointer; 
+  &:hover { 
+    background-color: #f0f0f0; 
+  } 
+`;
+
+export const FileDownloadButton = styled.button`
+  padding: 0.94rem 1.25rem; 
+  border: none; 
+  color: #0056F6;
+  cursor: pointer;
+`
