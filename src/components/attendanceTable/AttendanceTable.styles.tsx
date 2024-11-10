a71ceb2b826e2dca32a31bddad5158a0225b69c3
joonyoungchoi0801 <joeplay0801@gmail.com>
2024-11-10 18:40:47 +0900

    Feat: attendance table 수정

--- src/components/attendanceTable/AttendanceTable.styles.tsx
+++ src/components/attendanceTable/AttendanceTable.styles.tsx
@@ -1,45 +1,69 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const TableHeader = styled.div`
+export const Table = styled.table`
   display: flex;
-  height: 3.75rem;
-  padding: 0.625rem;
-  align-items: center;
-  gap: 1.375rem;
-  background: #F9F9F9;
+  width: 102rem;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 100%;
+  min-width: 102rem;
+  border: 0.1rem solid #e7e7e7;
+  background: var(--color-white);
 `;
 
-export const SeachContainer = styled.div`
+export const TableHeader = styled.thead`
   display: flex;
-  flex-direction: space-between;
-  width: 10.875rem;
-  height: 2.5rem;
-  padding: 0.3125rem 1.25rem;
   align-items: center;
-  gap: 0.625rem;
-  border-radius: 0.3125rem;
+  gap: 1.375rem;
+  background: #f9f9f9;
+  padding: 1rem;
+  width: 100%;
+`;
+
+export const SearchContainer = styled.th`
+  display: flex;
+  width: 17.4rem;
+  height: 4rem;
+  padding: 0.5rem 3rem;
+  align-items: center;
+  border-radius: 0.5rem;
   background: var(--color-gray);
+  position: relative;
 `;
 
 export const SearchIcon = styled.img`
-  width: 1.125rem;
-  height: 1.125rem;
+  position: absolute;
+  width: 1.5rem;
+  height: 1.5rem;
+  top: 50%;
+  transform: translateY(-50%);
+  left: 1rem;
 `;
 
 export const SearchInput = styled.input`
-  width: 7.875rem;
+  width: 100%;
+  height: 100%;
   border: transparent;
   background-color: var(--color-gray);
+  outline: none;
+  &::placeholder {
+    color: #999;
+    font-size: 1.2rem;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+    margin-left: 4.8rem;
+  }
 `;
 
-export const Pagination = styled.div`
+export const Pagination = styled.th`
   display: flex;
   align-items: center;
   justify-content: space-between;
   width: 100%;
 `;
 
-export const PaginationItem = styled.div`
+export const PaginationItem = styled.th`
   flex-grow: 1;
   text-align: center;
   font-size: 1.25rem;
@@ -48,6 +72,53 @@ export const PaginationItem = styled.div`
 
 export const ArrowButton = styled.img`
   cursor: pointer;
+  width: 1.5rem;
+  height: 1.5rem;
+  margin-right: 1.38rem;
+  margin-left: 1.38rem;
+`;
+
+export const TableBody = styled.tbody`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  overflow-y: auto;
+`;
+
+export const TableRow = styled.tr`
+  display: flex;
+  width: 100%;
+  border-bottom: 0.1rem solid #e7e7e7;
+  gap: 6.5rem;
+  padding: 1rem;
+  &:last-child {
+    border-bottom: none;
+  }
+`;
+
+export const StudentName = styled.td`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  width: 17.4rem;
+  color: #000;
+  font-size: 2rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+`;
+
+export const CheckBox = styled.input`
+  width: 2rem;
+  height: 2rem;
+`;
+
+export const StatusIcon = styled.img`
+  width: 2rem;
+  height: 2rem;
+`;
+
+export const Blank = styled.th`
   width: 1.5rem;
   height: 1.5rem;
   margin-right: 1.38rem;
