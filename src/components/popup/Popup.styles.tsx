49c68681a071ebde2ee3c63eb8ba881db6edbbc2
sohyun lim <limgm7@gmail.com>
2024-11-10 19:11:04 +0900

    feat: 수정/삭제 팝업창 컴포넌트 추가

---
+++ src/components/popup/Popup.styles.tsx
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+export const PopupContainer = styled.div`
+  position: relative;
+`;
+export const Menu = styled.div`
+  position: absolute;
+  top: 100%;
+  left: 0;
+  margin-top: 0.5rem;
+  width: 16rem;
+  background-color: #fff;
+  border-radius: 1rem;
+  box-shadow: 0px 0.4rem 0.8rem rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+  align-items: flex-start;
+  flex-direction: column;
+`;
+
+export const MenuItem = styled.button<{ isDelete?: boolean }>`
+  padding: 1.5rem 2rem;
+  color: '#000';
+  background-color: ${(props) => (props.isDelete ? '#f5faff' : '#fff')};
+  border: none;
+  cursor: pointer;
+  text-align: left;
+  justify-content: center;
+  align-items: flex-start;
+  align-self: stretch;
+  gap: 1rem;
+  width: 100%;
+  font-size: 2rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+
+  &:hover {
+    background-color: #f2f5fc;
+    color: var(--color-blue);
+  }
+`;
