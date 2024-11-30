da7133ae9d8ce8244ed29127f19012fa1943d499
roor <02tladbwjd@naver.com>
2024-11-30 13:41:00 +0900

    feat: 학생출결관리 페이지 헤더 추가

--- src/pages/attendance/Attendance.styles.tsx
+++ src/pages/attendance/Attendance.styles.tsx
@@ -1,10 +1,37 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  padding: 2.5rem;
   background-color: var(--color-white);
   width: 100%;
   position: relative;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  border-bottom: 0.1rem solid #e6e6e6;
+  width: 100%;
+  align-items: center;
+  justify-content: flex-start; /* 왼쪽 정렬 */
+  gap: 4.3rem; /* 버튼 간 간격 */
+  padding: 2.45rem 5rem;
+`;
+
+export const TabButton = styled.button<{ isActive: boolean }>`
+  border: none;
+  background: none;
+  font-size: 2rem;
+  font-style: normal;
+  line-height: normal;
+  color: ${({ isActive }) =>
+    isActive
+      ? 'var(--color-blue)'
+      : 'var(--color-black)'}; /* 활성화된 탭 색상 */
+  font-weight: ${({ isActive }) =>
+    isActive ? 700 : 600}; /* 활성화된 탭 굵기 */
+  cursor: pointer;
+  &:hover {
+    color: #007bff; /* 호버 시 색상 변경 */
+  }
 `;
 
 export const AttendanceTitle = styled.h1`
