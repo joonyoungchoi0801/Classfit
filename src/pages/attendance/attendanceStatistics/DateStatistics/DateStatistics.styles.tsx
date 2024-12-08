70231c851fae74893ea5065ce512a39822f1d375
roor <02tladbwjd@naver.com>
2024-12-08 17:46:36 +0900

    feat: 날짜별 통계 테이블 헤더

--- src/pages/attendance/attendanceStatistics/DateStatistics/DateStatistics.styles.tsx
+++ src/pages/attendance/attendanceStatistics/DateStatistics/DateStatistics.styles.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  background-color: #f9f9f9;
+  background-color: #fafafa;
   border: 1px solid #ddd;
   width: 100%;
   padding: 2rem 5rem;
@@ -36,4 +36,95 @@ export const PaginationItem = styled.div`
   font-size: 1.8rem;
   font-style: normal;
   font-weight: 600;
+`;
+
+export const Table = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 100%;
+  min-height: 38rem;
+  margin-top: 3rem;
+`;
+
+export const TableHeader = styled.div`
+  display: flex;
+  width: 100%;
+  height: 3rem;
+  position: relative;
+`;
+
+export const DropdownClass = styled.div`
+  display: flex;
+  background: #EDEDED;
+  width: 24.5rem;
+  height: 3rem;
+  align-items: center;
+  padding-left: 0.76rem;
+  padding-right: 0.76rem;
+  justify-content: space-between;
+  position: relative;
+  border-radius: 0.3807rem;
+  cursor: pointer;
+`;
+
+export const Placeholder = styled.span`
+  font-size: 1.4rem;
+  font-style: normal;
+  font-weight: 600;
+  color: #CACACA;
+`;
+
+export const DropdownButton = styled.img`
+  width: 1.5058rem;
+  height: 1.5058rem;
+`;
+
+export const DropdownList = styled.ul`
+  position: absolute;
+  top: 100%;
+  left: 0;
+  width: 20.1rem;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 0rem 0rem 1rem 1rem;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  z-index: 1;
+  text-align: left;
+`;
+
+export const DropdownItem = styled.li`
+  display: flex;
+  padding: 1rem 2rem;
+  color: var(--color-black);
+  font-size: 1.8rem;
+  font-style: normal;
+  font-weight: 500;
+
+  cursor: pointer;
+  &:hover {
+    background-color: #DFEBFF;
+  }
+`;
+
+export const PaginationContainer = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  margin-left: 2rem;
+`;
+
+export const DatePaginationItem = styled.div`
+  text-align: center;
+  font-size: 1.8rem;
+  font-style: normal;
+  font-weight: 600;
+  width: 12rem;
+`;
+
+export const StatisticsContainer = styled.div`
+
 `;
--- src/pages/attendance/attendanceStatistics/DateStatistics/index.tsx
+++ src/pages/attendance/attendanceStatistics/DateStatistics/index.tsx
@@ -1,7 +1,9 @@
 import * as S from './DateStatistics.styles';
 import paginationLeft from '@/assets/attendanceTable/paginationLeft.svg';
 import paginationRight from '@/assets/attendanceTable/paginationRight.svg';
-import { useState } from 'react';
+import dropdwon from '@/assets/buttonIcon/dropdown.svg';
+
+import { useState, useEffect } from 'react';
 
 const getLastSixMonths = (offset = 0) => {
   const currentDate = new Date();
@@ -22,9 +24,36 @@ const getLastSixMonths = (offset = 0) => {
   return { months: months.reverse(), currentMonth };
 };
 
+// 선택한 월의 첫날부터 날짜와 요일을 계산하는 함수
+const getDatesInMonth = (year: number, month: number) => {
+  const firstDayOfMonth = new Date(year, month - 1, 1); // 해당 월의 첫째 날
+  const lastDayOfMonth = new Date(year, month, 0); // 해당 월의 마지막 날
+
+  const daysInMonth = [];
+  let currentDate = firstDayOfMonth;
+
+  while (currentDate <= lastDayOfMonth) {
+    // getDay()는 일요일을 0으로 반환하므로, 이를 조정하여 월요일을 0, 일요일을 6으로 설정
+    const weekday = (currentDate.getDay() === 0 ? 6 : currentDate.getDay() - 1);
+
+    daysInMonth.push({
+      date: currentDate.getDate(),
+      weekday, // 수정된 요일 값 (월요일=0, 일요일=6)
+    });
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+
+  return daysInMonth;
+};
+
 function DateStatistics() {
   const [monthOffset, setMonthOffset] = useState(0);
   const { months, currentMonth } = getLastSixMonths(monthOffset);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [datesInMonth, setDatesInMonth] = useState<{ date: number, weekday: number }[]>([]);
+  const [currentPage, setCurrentPage] = useState(0); // 현재 페이지 (7일씩 끊어서)
+
+  const subclass = [{ class: "1학년 A반" }, { class: "2학년 A반" }];
 
   const handlePrevMonth = () => {
     if (monthOffset < 5) {
@@ -36,6 +65,36 @@ function DateStatistics() {
     if (monthOffset > 0) {
       setMonthOffset(monthOffset - 1);
     }
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage > 0) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if ((currentPage + 1) * 7 < datesInMonth.length) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  useEffect(() => {
+    const currentMonthData = new Date();
+    const currentYear = currentMonthData.getFullYear();
+    setDatesInMonth(getDatesInMonth(currentYear, currentMonth));
+    setCurrentPage(0); // 월이 변경되면 페이지를 1일부터 보도록 설정
+  }, [currentMonth]);
+
+  // 현재 페이지에 해당하는 7일씩 끊어서 날짜들만 반환
+  const getCurrentWeekDates = () => {
+    const startIdx = currentPage * 7;
+    const endIdx = startIdx + 7;
+    return datesInMonth.slice(startIdx, endIdx);
   };
 
   return (
@@ -52,12 +111,60 @@ function DateStatistics() {
         </S.PaginationItem>
         <S.ArrowButton
           src={paginationRight}
-          alt='Next Week'
+          alt='Next Month'
           onClick={handleNextMonth}
         />
       </S.TitleWrapper>
 
+      <S.Table>
+        <S.TableHeader>
+          <S.DropdownClass onClick={toggleDropdown}>
+            <S.Placeholder>구분</S.Placeholder>
+            <S.DropdownButton src={dropdwon} alt='dropdown icon' />
+          </S.DropdownClass>
+          {isDropdownOpen && (
+            <S.DropdownList>
+              {subclass.map((item, index) => (
+                <S.DropdownItem
+                  key={index}
+                >
+                  {item.class}
+                </S.DropdownItem>
+              ))}
+            </S.DropdownList>
+          )}
+          <S.PaginationContainer>
+            <S.ArrowButton
+              src={paginationLeft}
+              alt='Previous Week'
+              onClick={handlePrevPage}
+            />
+            {getCurrentWeekDates().map((dateInfo, index) => (
+              <S.DatePaginationItem key={index}>
+                {`${currentMonth}/${dateInfo.date}(${['월', '화', '수', '목', '금', '토', '일'][dateInfo.weekday]})`}
+              </S.DatePaginationItem>
+            ))}
+            <S.ArrowButton
+              src={paginationRight}
+              alt='Next Week'
+              onClick={handleNextPage}
+            />
+          </S.PaginationContainer>
+        </S.TableHeader>
 
+        <S.StatisticsContainer>
+
+        </S.StatisticsContainer>
+        <S.StatisticsContainer>
+
+        </S.StatisticsContainer>
+        <S.StatisticsContainer>
+
+        </S.StatisticsContainer>
+        <S.StatisticsContainer>
+
+        </S.StatisticsContainer>
+      </S.Table>
     </S.Container>
   );
 }
