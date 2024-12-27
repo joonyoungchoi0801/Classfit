9934417ce00a61f535c82c1de91e083222a063ad
roor <02tladbwjd@naver.com>
2024-12-28 03:01:21 +0900

    feat: 일정 등록 모달 컴포넌트

---
+++ src/components/modal/scheduleRegisterModal/ScheduleRegisterModal.styles.tsx
@@ -0,0 +1,216 @@
+import styled from 'styled-components';
+import React from 'react';
+
+export const ModalWrapper = styled.div`
+  position: fixed;
+  display: flex;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+`;
+
+export const ModalContainer = styled.div`
+  position: fixed;
+  width: 50rem;
+  padding: 2rem 2.5rem;
+  background: var(--color-white);
+  border-radius: 1rem;
+  box-shadow: 0px 4px 15px 0px rgba(0, 0, 0, 0.15);
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+export const Title = styled.h2`
+  margin-bottom: 2rem;
+  font-size: 2rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+`;
+
+export const Divider = styled.div`
+  margin: 1rem 0;
+  border: none;
+  border-top: 1px solid #A7A7A7;
+`;
+
+export const OptionGroup = styled.div`
+  display: flex;
+  gap: 1rem;
+  margin-top: 2rem;
+`;
+
+export const Button = styled.button`
+  padding: 0.6rem 1.4rem;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  border-radius: 0.5rem;
+  color: var(--color-white);
+  font-size: 1.4rem;
+  font-style: normal;
+  font-weight: 600;
+  cursor: pointer;
+
+  &:first-child {
+    background: var(--color-blue);
+    margin-left: 0;
+  }
+  &:last-child {
+    background: #E5E5E5;
+    margin-right: 0;
+  }
+`;
+
+export const FormGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  justify-content: space-between;
+  flex: 1 0 0;
+`;
+
+export const Label = styled.label`
+  font-size: 1.6rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  margin-bottom: 1rem;
+  margin-top: 2rem;
+`;
+
+export const Input = styled.input`
+  padding: 1rem 2rem;
+  border-radius: 1rem;
+  border: 1px solid #DEDEDE;
+  font-size: 1.4rem;
+  font-style: normal;
+  font-weight: 600;
+  width: 100%;
+
+  ::placeholder {
+    color: #CACACA;
+    font-size: 1.4rem;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 2.4rem;
+  }
+`;
+
+export const Row = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+
+  span {
+    margin: 0 8px;
+  }
+`;
+
+export const SelectWrapper = styled.div`
+  position: relative;
+  width: 100%;
+`;
+
+export const Select = styled.div<{ hasValue: boolean }>`
+  width: 100%;
+  padding: 1rem 1rem 1rem 2rem;
+  font-size: 1.4rem;
+  border-radius: 1rem;
+  border: 1px solid #DEDEDE;
+  color: ${({ hasValue }) => (hasValue ? '#000' : '#CACACA')};
+  font-style: normal;
+  font-weight: 600;
+  line-height: 2.4rem;
+  cursor: pointer;
+`;
+
+export const DropdownIcon = styled.img`
+  position: absolute;
+  top: 50%;
+  right: 2rem;
+  transform: translateY(-50%);
+  width: 2.4rem;
+  height: 2.4rem;
+  color: var(--color-black);
+`;
+
+export const Options = styled.div`
+  position: absolute;
+  top: 100%;
+  left: 0;
+  width: 100%;
+  max-height: 200px;
+  overflow-y: auto;
+  border: 1px solid #DEDEDE;
+  border-radius: 1rem;
+  background-color: white;
+  z-index: 1;
+`;
+
+export const Option = styled.div`
+  padding: 1rem 2rem;
+  font-size: 1.6rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 2.4rem;
+  color: var(--color-black);
+  cursor: pointer;
+
+  &:hover {
+    background-color: #DFEBFF;
+  }
+`;
+
+export const ColumnRow = styled.div`
+  display: flex;
+  gap: 1rem;
+  flex-direction: column;
+
+  span {
+    font-size: 1rem;
+    margin: 0 1.5rem;
+  }
+`;
+
+export const DateWrapper = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  width: 100%;
+`;
+
+export const DateInputWrapper = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+export const CheckboxGroup = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 1rem;
+  margin-bottom: 2rem;
+
+  span {
+    font-size: 1.4rem;
+  }
+`;
+
+export const Checkbox = styled.input.attrs({ type: 'checkbox' })`
+  width: 1.6rem;
+  height: 1.6rem;
+  margin-right: 0.4rem;
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 1rem;
+`;
---
+++ src/components/modal/scheduleRegisterModal/index.tsx
@@ -0,0 +1,115 @@
+import React, { useState } from 'react';
+import * as S from './ScheduleRegisterModal.styles';
+import type { ScheduleRegisterModalProps } from './ScheduleRegisterModal.types';
+import dropdown from '@/assets/buttonIcon/dropdown.svg';
+import Button from '@/components/button';
+
+const ScheduleRegisterModal: React.FC<ScheduleRegisterModalProps> = ({ isOpen, onClose }) => {
+  const [calendarValue, setCalendarValue] = useState('');
+  const [categoryValue, setCategoryValue] = useState('');
+  const [isCalendarOpen, setIsCalendarOpen] = useState(false);
+  const [isCategoryOpen, setIsCategoryOpen] = useState(false);
+
+  const handleCalendarChange = (value: string) => {
+    setCalendarValue(value);
+    setIsCalendarOpen(false);
+  };
+
+  const handleCategoryChange = (value: string) => {
+    setCategoryValue(value);
+    setIsCategoryOpen(false);
+  };
+
+  if (!isOpen) return null;
+
+  return (
+    <S.ModalWrapper>
+      <S.ModalContainer>
+        <S.Title>일정 등록</S.Title>
+        <S.Divider />
+        <S.OptionGroup>
+          <S.Button>스케줄</S.Button>
+          <S.Button>할일</S.Button>
+        </S.OptionGroup>
+
+        <S.FormGroup>
+          <S.Label>일정명</S.Label>
+          <S.Input type="text" placeholder="일정명 입력" />
+        </S.FormGroup>
+
+        <S.Row>
+          <S.FormGroup>
+            <S.Label>캘린더</S.Label>
+            <S.SelectWrapper>
+              <S.Select onClick={() => setIsCalendarOpen(!isCalendarOpen)} hasValue={Boolean(calendarValue)}>
+                {calendarValue || '캘린더 선택'}
+                <S.DropdownIcon src={dropdown} alt="dropdown icon" />
+              </S.Select>
+              {isCalendarOpen && (
+                <S.Options>
+                  <S.Option onClick={() => handleCalendarChange('내 캘린더')}>
+                    내 캘린더
+                  </S.Option>
+                  <S.Option onClick={() => handleCalendarChange('공용 캘린더')}>
+                    공용 캘린더
+                  </S.Option>
+                </S.Options>
+              )}
+            </S.SelectWrapper>
+          </S.FormGroup>
+          <S.FormGroup>
+            <S.Label>카테고리</S.Label>
+            <S.SelectWrapper>
+              <S.Select onClick={() => setIsCategoryOpen(!isCategoryOpen)} hasValue={Boolean(categoryValue)} >
+                {categoryValue || '카테고리 선택'}
+                <S.DropdownIcon src={dropdown} alt="dropdown icon" />
+              </S.Select>
+              {isCategoryOpen && (
+                <S.Options>
+                  <S.Option onClick={() => handleCategoryChange('카테고리1')}>
+                    카테고리 1
+                  </S.Option>
+                  <S.Option onClick={() => handleCategoryChange('카테고리 2')}>
+                    카테고리 2
+                  </S.Option>
+                </S.Options>
+              )}
+            </S.SelectWrapper>
+          </S.FormGroup>
+        </S.Row>
+
+        <S.ColumnRow>
+          <S.Label>일시</S.Label>
+          <S.DateWrapper>
+            <S.DateInputWrapper>
+              <S.Input type="date" placeholder="날짜 선택" />
+            </S.DateInputWrapper>
+            <span>—</span>
+            <S.DateInputWrapper>
+              <S.Input type="date" placeholder="날짜 선택" />
+            </S.DateInputWrapper>
+          </S.DateWrapper>
+        </S.ColumnRow>
+
+        <S.CheckboxGroup>
+          <S.Checkbox type="checkbox" />
+          <span>종일</span>
+        </S.CheckboxGroup>
+
+        <S.ButtonWrapper>
+          <Button
+            title='취소'
+            textColor='var(--color-black)'
+            backgroundColor='var(--color-white)'
+            isBorder={true}
+            borderColor='#D7D7D7'
+            onClick={onClose}
+          />
+          <Button title='저장' />
+        </S.ButtonWrapper>
+      </S.ModalContainer>
+    </S.ModalWrapper>
+  );
+};
+
+export default ScheduleRegisterModal;
