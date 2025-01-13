c8bcd6452c223f78185d8e73ce50cb8b2043a4d0
sohyun lim <limgm7@gmail.com>
2025-01-14 02:12:57 +0900

    fix: provider 임시 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { QueryClient } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import Router from './Router';
 
 const queryClient = new QueryClient();
 
 function App() {
-  return <Router />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
